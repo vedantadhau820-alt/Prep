d9fb72320be0c73cf65bf15a4d6c8d684322e502
vedantadhau820-alt <vedantadhau820@gmail.com>
2026-03-08 04:03:31 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = "prep-cache-v1"
+
+const ASSETS = [
+"/",
+"/index.html",
+"/manifest.json",
+"/icon.png",
+]
+
+self.addEventListener("install", e => {
+
+e.waitUntil(
+caches.open(CACHE_NAME)
+.then(cache => cache.addAll(ASSETS))
+)
+
+})
+
+self.addEventListener("fetch", e => {
+
+e.respondWith(
+caches.match(e.request)
+.then(res => res || fetch(e.request))
+)
+
+})
